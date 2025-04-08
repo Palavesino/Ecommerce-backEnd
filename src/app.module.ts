96f921d3f927adac19da4482d2caf49777173e77
Facundo <palavesinofacundo@gmail.com>
2025-04-07 22:04:12 -0300

    Add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { databaseProviders } from './config/database.providers';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { ModulesModule } from './modules/module.modules';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseProviders)],
+  imports: [TypeOrmModule.forRoot(databaseProviders),ModulesModule],
   controllers: [],
   providers: [
     {
